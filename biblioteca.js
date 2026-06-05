4786daf31688b2c531ab7a1027a20a46f7dca844
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:15:53 -0300

    Delete biblioteca.js

--- biblioteca.js
+++
@@ -1,4 +0,0 @@
-// functions/api/biblioteca.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('biblioteca');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
