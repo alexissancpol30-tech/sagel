b1fc2428be849d73f7e3ef4ac33af17cf83b5cf4
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:16:03 -0300

    Delete comisiones.js

--- comisiones.js
+++
@@ -1,4 +0,0 @@
-// functions/api/comisiones.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('comisiones');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
