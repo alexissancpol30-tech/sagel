b1523531a9cb25e44815acd408f7d1a4704935bc
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:15:33 -0300

    Delete actores.js

--- actores.js
+++
@@ -1,4 +0,0 @@
-// functions/api/actores.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('actores');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
