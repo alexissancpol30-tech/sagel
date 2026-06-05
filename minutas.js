8f0b0580c263600e5b259c1e5d2b13e8b64a1626
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:16:38 -0300

    Delete minutas.js

--- minutas.js
+++
@@ -1,4 +0,0 @@
-// functions/api/minutas.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('minutas');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
