e06ae784bd8fb4226f5c59904bc3960fde24f181
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:19:54 -0300

    Delete sesiones.js

--- sesiones.js
+++
@@ -1,4 +0,0 @@
-// functions/api/sesiones.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('sesiones');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
