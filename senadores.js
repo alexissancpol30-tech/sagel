5831496e1a335bb942d1195f761148a0a6234f66
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:17:00 -0300

    Delete senadores.js

--- senadores.js
+++
@@ -1,4 +0,0 @@
-// functions/api/senadores.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('senadores');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
