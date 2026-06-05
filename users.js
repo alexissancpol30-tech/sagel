129d6bf0f7e4744e9b5abe6f8c37f8ff00bd1293
alexissancpol30-tech <alexissan.cpol30@gmail.com>
2026-06-05 02:20:22 -0300

    Delete users.js

--- users.js
+++
@@ -1,4 +0,0 @@
-// functions/api/users.js
-import { makeCRUD } from './_crud.js';
-const { onRequestGet, onRequestPost, onRequestPut, onRequestDelete } = makeCRUD('users');
-export { onRequestGet, onRequestPost, onRequestPut, onRequestDelete };
